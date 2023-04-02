be27e90594c0457822dcb88a8e71a8b510947416
Aviv Liberman <118883083+liberman809@users.noreply.github.com>
2023-04-02 23:32:48 +0300

    Add files via upload

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   dbURL: '  mongodb+srv://liberman809:<liberman25>@letstalke.pfz4ctb.mongodb.net/?retryWrites=true&w=majority',
-  dbName : 'letsTalke '
+  dbName : 'letsTalke'
 }
 console.log('a')
